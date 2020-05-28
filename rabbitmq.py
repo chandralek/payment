42b224929a791610683d50fa3ea0ce894dcd0311
Chandralekha.s <chandralekha.s@sophos.com>
2020-05-28 17:56:00 +0530

    Updating payment module

--- rabbitmq.py
+++ rabbitmq.py
@@ -13,7 +13,7 @@ class Publisher:
 
     def __init__(self, logger):
         self._logger = logger
-        self._params? = pika.connection.ConnectionParameters(
+        self._params = pika.connection.ConnectionParameters(
             host=self.HOST,
             virtual_host=self.VIRTUAL_HOST,
             credentials=pika.credentials.PlainCredentials(self.RUSERNAME, self.RPASSWORD))
